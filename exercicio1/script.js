b3852f520ac7c7b41a78c18684409f94bdca302c
DioguinhoSantos <diiogosantos@live.com>
2022-08-09 18:16:07 -0300

    Barbosa-A-Diogo-Santos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,5 @@
-const num = //seu numero aqui
+const num = Number(prompt(`Insira o número`));
+
+num % 2 == 0 || num % 3 == 0 ? console.log(`número divisível por 2 ou 3`) : console.log(`número não divisível por 2 e/ou 3`);
+
 
--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,15 +1,31 @@
-let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
+const nacionalidade = String(prompt(`Escreva aqui sua nacionalidade!`)).toLowerCase();
 
-if(nacionalidade === "brasileira"){
-    console.log("a pessoa é do Brasil!");
-} else if(nacionalidade === "argentina"){
-    console.log("a pessoa é da Argentina!");
-} else if(nacionalidade === "uruguaia"){
-    console.log("a pessoa é do Uruguai!");
-} else if(nacionalidade === "chilena"){
-    console.log("a pessoa é do Chile!");
-} else if(nacionalidade === "colombiana"){
-    console.log("a pessoa é da Colômbia!");
-} else{
-    console.log("nacionalidade não encontrada")
-}
+//let nacionalidadeCorrigida = nacionalidade.toLowerCase();
+
+switch(nacionalidade){
+
+case `brasileira`:
+    console.log(`Sua nacionalidade é: ${nacionalidade}!`)
+    break;
+case `argentina`:
+    console.log(`Sua nacionalidade é: ${nacionalidade} !`)
+    break;
+case `uruguaia`:
+    console.log(`Sua nacionalidade é: ${nacionalidade}!`)
+    break;
+case `chilena`:
+    console.log(`Sua nacionalidade é: ${nacionalidade}!`)
+    break;
+case `colombiana`:
+    console.log(`Sua nacionalidade é: ${nacionalidade}!`)
+    break;
+default:
+    console.log(`Nacionalidade não encontrada!`) }
+/*nacionalidade === `argentina` ||
+nacionalidade === `uruguaia` ||
+nacionalidade === `chilena` ||
+nacionalidade === `colombiana`){
+    console.log(`Sua nacionalidade é: ${nacionalidade}`)
+}else{
+    console.log(`Nacionalidade não encontrada`);
+}*/
